52b188eb99beef8abce1dda62c5229a9279aa259
officialtech <officialtech@protonmail.com>
2021-03-21 20:33:18 +0530

    django secret key removed

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^x6-k=eby*ylongp_j49%ho5di$-=-3nok1u4778&8iaz*^1@e'
+SECRET_KEY = config(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=Csv())
 
 ALLOWED_HOSTS = []
 
